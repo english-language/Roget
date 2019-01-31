c7a274033a27a40821791a6b98fb0f4efe25317e
alxfed <alxfed@gmail.com>
2019-01-31 10:42:51 -0600

    file

--- splitting_of_pg22.py
+++ splitting_of_pg22.py
@@ -3,6 +3,8 @@ Splitting of raw pg22.txt into formatted individual files
 
 the original of the txt is at: http://www.gutenberg.org/cache/epub/22/pg22.txt
 
+Regular expressions reference pages: https://docs.python.org/3/library/re.html?highlight=regular%20expressions
+and HOWTO: https://docs.python.org/3/howto/regex.html#regex-howto
 """
 
 """
@@ -38,10 +40,33 @@ The pages are marked as:
 
 These marks should probably be removed first.
 """
+import re
+
+# the % symbol in the beginning of the line
+
+be = re.compile('%\n')   # beginning or end of a multi-line
+                        # title of class, section, part
+
+beg = True             # this is the end of the multi-line
+
+roget = open('roget.txt', 'r')
+
+# the main cycle:
+for line in roget:
+    if re.match(be, line) and beg:
+        next = roget.readline()
+        print(next)
+        beg = False
+    elif not beg: beg = True
 
 
-for line in open('roget.txt'):
-    pass
+# the symbol at the  beginning of the line is detected best
+# by re.match(), that's what should be used for % and <--
+#
+#
+
+
+
 
 """
 
